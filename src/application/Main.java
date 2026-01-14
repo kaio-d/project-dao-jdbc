3a3dfc7831861c10448f14903317c8dc3b3b6aae
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2026-01-14 17:47:12 -0300

    Create Department Class

---
+++ src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Department d1 = new Department(1, "ADM");
+		System.out.println(d1);
+
+	}
+
+}
