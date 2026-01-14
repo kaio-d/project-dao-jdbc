8b6cff357cb659b227798445b78c9e5c36f34e38
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2026-01-14 18:03:49 -0300

    Create Seller class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Department d1 = new Department(1, "ADM");
-		System.out.println(d1);
 
+		LocalDate birthDate = LocalDate.parse("2001-07-07");
+		Seller s1 = new Seller(21, "Kaio", "kaio@teste.com", birthDate, 3000.0, d1);
+		System.out.println(s1);
 	}
 
 }
