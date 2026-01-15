106858a8b6d46ccd5ef4bc48cc47baeeb83cbc9c
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2026-01-14 21:17:55 -0300

    reusing instantiation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,5 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
 import model.entities.Seller;
@@ -12,6 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		SellerDAO sellerDAO = DAOFactory.createSellerDao();
 		
+		System.out.println("Execute method findById: ");
 		Seller seller = sellerDAO.findByID(3);
 		
 		System.out.println(seller);
