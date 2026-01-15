346d329b88541a505d0434d75220e2ded3d08921
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2026-01-15 20:22:30 -0300

    create a method insert

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import model.dao.DAOFactory;
@@ -30,6 +33,21 @@ public class Main {
 		List<Seller> sellers = sellerDAO.findAll();
 		for(Seller s : sellers) {
 			System.out.println(s);
+		}
+		
+		System.out.println();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println("Execute method insert: ");
+		String stringBirthDate = "2001-07-07";
+		try {
+		    java.util.Date birthDate = sdf.parse(stringBirthDate);
+		    
+		    Seller newSeller = new Seller(null, "Luis", "luis@gmail.com", birthDate,
+					4000.0, dep);
+			sellerDAO.insert(newSeller);
+			System.out.println("Insert a new seller! ID = " + newSeller.getId());
+		} catch (ParseException e) {
+		    e.printStackTrace();
 		}
 
 	}
